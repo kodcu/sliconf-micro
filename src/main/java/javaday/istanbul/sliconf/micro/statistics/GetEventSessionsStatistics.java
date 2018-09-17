039de0fc81531ce78555fc3e1f3130d912a49bb9
Murat Özer <murat.ozer@yahoo.com>
2018-09-17 23:05:02 +0300

    Bir anketteki tüm konuşmalar hakkındaki istatistiksel bilgilere ulaşmak için bir endpoint yazıldı

---
+++ src/main/java/javaday/istanbul/sliconf/micro/statistics/GetEventSessionsStatistics.java
@@ -0,0 +1,49 @@
+package javaday.istanbul.sliconf.micro.statistics;
+
+import io.swagger.annotations.*;
+import javaday.istanbul.sliconf.micro.model.response.ResponseMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@AllArgsConstructor
+@Api(value = "survey", authorizations = {@Authorization(value = "Bearer" )})
+@Path("/service/events/:eventKey/statistics/sessions")
+@Produces("application/json")
+@Component
+public class GetEventSessionsStatistics implements Route {
+
+    private final StatisticsService statisticsService;
+
+    @POST
+    @ApiOperation(value = "Adds a new survey to a event", nickname = "AddNewSurveyRoute")
+    @ApiImplicitParams({ //
+            @ApiImplicitParam(required = true, dataType = "string", name = "token", paramType = "header",
+                    example = "Authorization: Bearer <tokenValue>"), //
+            @ApiImplicitParam(required = true, dataType= "string", name = "eventKey", paramType = "request"), //
+
+    }) //
+    @ApiResponses(value = { //
+            @ApiResponse(code = 200, message = "Success", response = ResponseMessage.class), //
+            @ApiResponse(code = 400, message = "Invalid input data", response = ResponseMessage.class), //
+            @ApiResponse(code = 401, message = "Unauthorized", response = ResponseMessage.class), //
+            @ApiResponse(code = 404, message = "User not found", response = ResponseMessage.class) //
+    })
+
+
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        String eventKey = request.params("eventKey");
+        ResponseMessage responseMessage = new ResponseMessage();
+        responseMessage = statisticsService.getEventSessionsStatistics(eventKey);
+
+        return responseMessage;
+    }
+}
---
+++ src/main/java/javaday/istanbul/sliconf/micro/statistics/StatisticsService.java
@@ -0,0 +1,51 @@
+package javaday.istanbul.sliconf.micro.statistics;
+
+import javaday.istanbul.sliconf.micro.model.event.Event;
+import javaday.istanbul.sliconf.micro.model.event.Speaker;
+import javaday.istanbul.sliconf.micro.model.event.agenda.AgendaElement;
+import javaday.istanbul.sliconf.micro.model.response.ResponseMessage;
+import javaday.istanbul.sliconf.micro.service.event.EventRepositoryService;
+import javaday.istanbul.sliconf.micro.service.user.UserRepositoryService;
+import javaday.istanbul.sliconf.micro.survey.service.GeneralService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class StatisticsService {
+
+    private final EventRepositoryService eventRepositoryService;
+
+    private final GeneralService generalService;
+
+    private final EventSessionStatisticsDTO eventSessionStatisticsDTO;
+
+    ResponseMessage getEventSessionsStatistics(String eventKey) {
+
+        Event event = (Event) generalService.findEventByIdOrEventKey(eventKey).getReturnObject();
+        List<AgendaElement> agendaElements = new ArrayList<>(event.getAgenda());
+        List<Speaker> speakers = new ArrayList<>(event.getSpeakers());
+        List<EventSessionStatisticsDTO> eventSessionStatisticsDTOList = new ArrayList<>();
+
+        agendaElements.forEach(agendaElement -> {
+            speakers.forEach(speaker -> {
+                if(agendaElement.getSpeaker().equals(speaker.getId())) {
+                    eventSessionStatisticsDTO.setPhoto(speaker.getProfilePicture());
+                    String average = String.valueOf(agendaElement.getStar()) ;
+                    eventSessionStatisticsDTO.setAverage(average);
+                    eventSessionStatisticsDTO.setCount(String.valueOf(agendaElement.getVoteCount()));
+                    eventSessionStatisticsDTO.setTopic(agendaElement.getTopic());
+                    eventSessionStatisticsDTO.setWorkingAt(speaker.getWorkingAt());
+                    eventSessionStatisticsDTO.setSpeaker(speaker.getName());
+                    eventSessionStatisticsDTOList.add(eventSessionStatisticsDTO);
+                }
+            });
+        });
+
+        return new ResponseMessage(true, "Session Statistics Listed", eventSessionStatisticsDTOList);
+
+    }
+}
